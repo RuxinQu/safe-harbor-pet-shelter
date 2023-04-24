ad4d9cbe4db85e5db568ab7b892cab02f4aef8d1
Ruxin Qu <rosheenqu@gmail.com>
2023-04-23 23:03:01 -0400

    add aws credential

--- server/util/imageHelper.js
+++ server/util/imageHelper.js
@@ -6,6 +6,10 @@ require("dotenv").config();
 
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 // images is the key!
